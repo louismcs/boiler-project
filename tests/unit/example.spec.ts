d62ac3aba122c33ff31137575f2c1fdf5b24260a
Louis Slater <louismcs@hotmail.co.uk>
2019-05-29 20:51:40 +0100

    Changed from jest to mocha

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,9 @@
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import { expect } from 'chai'
+import { shallowMount } from '@vue/test-utils'
+import HelloWorld from '@/components/HelloWorld.vue'
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    });
-    expect("hi").toMatch("hi");
-  });
-});
+describe('HelloWorld.vue', () => {
+  it('Test test', () => {
+    console.log('TESTING');
+  })
+})
