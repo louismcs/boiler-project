57af51f0458eef22cc32db54ff656194d64b627e
Louis Slater <louismcs@hotmail.co.uk>
2019-05-29 15:58:25 +0100

    Setup subdomain functionality

--- server/app.js
+++ server/app.js
@@ -1,8 +1,12 @@
 var express = require('express');
+var subdomain = require('express-subdomain');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
+
+var apiRouter = require('./routes/api');
+
 var app = express();
 
 
@@ -11,8 +15,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(subdomain('api', apiRouter));
 app.use(express.static(path.join(__dirname, '/public/')));
 
-app.get(/.*/, (req, res) => /* : void */res.sendFile(__dirname + '/public/index.html'));
+app.get(/.*/, (req, res) /* : void */ =>
+    res.sendFile(__dirname + '/public/index.html'));
 
 module.exports = app;
---
+++ server/routes/api.js
@@ -0,0 +1,9 @@
+var Router = require('express').Router;
+var router = Router();
+
+/* GET home page. */
+router.get('/test', function(req, res, next) {
+    res.send('Hello API 2');
+});
+
+module.exports = router;
