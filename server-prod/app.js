a262a9c16ddd7e4dca66363145bfb2268d600953
Louis Slater <louismcs@hotmail.co.uk>
2019-05-29 19:39:11 +0100

    Added typescript compilation

--- server-prod/app.js
+++ server-prod/app.js
@@ -1,17 +1,33 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
-var app = express();
+"use strict";
 
+var express = require('express');
+
+var subdomain = require('express-subdomain');
+
+var path = require('path');
+
+var cookieParser = require('cookie-parser');
+
+var logger = require('morgan');
+
+var cors = require('cors');
+
+var apiRouter = require('./routes/api').default;
+
+var app = express();
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
+app.use(subdomain('api', apiRouter));
 app.use(express.static(path.join(__dirname, '/public/')));
-
-app.get(/.*/, (req, res) => /* : void */res.sendFile(__dirname + '/public/index.html'));
-
+app.get(/.*/, function (req, res) {
+  return (
+    /* : void */
+    res.sendFile(__dirname + '/public/index.html')
+  );
+});
 module.exports = app;
